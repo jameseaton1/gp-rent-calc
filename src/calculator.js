14ec864ff9613c4434f962de0b06a7c7c3412f69
jameseaton1 <jameseaton@hotmail.com>
2021-01-10 21:45:01 +0000

    ADDED the start to yearly data calculations

--- src/calculator.js
+++ src/calculator.js
@@ -2,8 +2,21 @@
 function calculate(monthlyRent, year) {
 
     let years = []
+    var yearlyRent = monthlyRent * 12;
+    var data = getData();
 
-    console.log(getData());
+    
+    data.forEach(function (year, index) {
+        // x * 1.04 = y, x= y/1.04
+        yearlyRent /= (1 + year.wageInflationRate);
+        year.rentDeflationTotal = yearlyRent;
+      });
+
+    data.reverse().forEach(function (year, index) {
+        year.rentInflationTotal = yearlyRent * (1 + year.wageInflationRate)
+      });
+
+    console.log(data);
     output = {monthlyRent, year};
 
 
@@ -13,8 +26,9 @@ function calculate(monthlyRent, year) {
 }
 
 function getData() {
-    return [  { year: 2012, rentInflationRate: 3.3, wageInflationRate: 0.6},
+    return [  
         { year: 2013, rentInflationRate: 10.5, wageInflationRate: 0.4},
+        { year: 2012, rentInflationRate: 3.3, wageInflationRate: 0.6}
                 
     ]
 }
