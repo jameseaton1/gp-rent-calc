24f1ee18404c517ca56339446324ab53ce9a3146
jameseaton1 <jameseaton@hotmail.com>
2021-01-10 21:16:41 +0000

    ADDED monthly and year response

--- src/calculator.js
+++ src/calculator.js
@@ -1,12 +1,22 @@
 
 function calculate(monthlyRent, year) {
 
+    let years = []
+
+    console.log(getData());
     output = {monthlyRent, year};
 
 
     return output;
 
 
+}
+
+function getData() {
+    return [  { year: 2012, rentInflationRate: 3.3, wageInflationRate: 0.6},
+        { year: 2013, rentInflationRate: 10.5, wageInflationRate: 0.4},
+                
+    ]
 }
 
 module.exports = calculate;   
